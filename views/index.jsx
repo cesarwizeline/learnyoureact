8d9a821634d129847b70a4ff632feb334cec7d91
cesarwizeline <cesar.romero@wizeline.com>
2022-01-25 15:13:47 -0600

    css

--- views/index.jsx
+++ views/index.jsx
@@ -16,7 +16,7 @@ class TodoList extends React.Component {
   render() {
     return (
       <div className="todoList">
-        <table style={{ border: '2px solid black;' }}>
+        <table style={style}>
           <tbody>
             <Todo title="Shopping">Milk</Todo>
             <Todo title="Hair cut">13:00</Todo>
@@ -66,3 +66,7 @@ class TodoForm extends React.Component {
     return <div className="todoForm">I am a TodoForm.</div>;
   }
 }
+
+let style = {
+  border: '2px solid black;',
+};
